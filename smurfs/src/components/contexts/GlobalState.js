5f24a424a1539599c48726dbdc659168ed13a990
Tasha <tashingsworth@gmail.com>
2020-06-26 19:33:39 -0400

    set up reducer and context

---
+++ smurfs/src/components/contexts/GlobalState.js
@@ -0,0 +1,42 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './reducers';
+
+export const initialState = {
+    editing: false,
+    isFetching: true,
+    error: "",
+    smurfs: []
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions
+    const fetchSuccess = (newState) => {
+        dispatch({
+            type: "FETCH_SUCCESS",
+            payload: newState,
+        });
+    };
+
+    const fetchError = (error) => {
+        dispatch({
+            type: "FETCH_ERROR",
+            payload: `Error ${error.res.status} : ${error.response.data}`
+        });
+    };
+
+    const editing = () => {
+        dispatch({
+            type: "EDITING"
+        });
+
+        return (
+            <GlobalContext.Provider value={{ fetchSuccess, fetchError, state, editing}}> {children} </GlobalContext.Provider>
+        );
+    }
+};
