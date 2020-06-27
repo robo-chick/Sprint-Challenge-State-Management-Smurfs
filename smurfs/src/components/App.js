b07942841dbcdea3517869a7d4a939b521288e20
Tasha <tashingsworth@gmail.com>
2020-06-26 20:19:31 -0400

    installed axios, created SmurfForm to add Smurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import React, {useContext, useEffect} from 'react';
+import {GlobalContext} from '../contexts/GlobalState';
+import axios from 'axios';
+import './App.css';
+import SmurfForm from './SmurfForm';
+
+const App = () => {
+  const {fetchSuccess, fetchError} = useContext(GlobalContext);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        fetchSuccess(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        fetchError(err.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      <SmurfForm />
+    </div>
+  )
 }
 
 export default App;
